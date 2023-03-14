bdc24bfd2532bce82f1a63974b1803cce35c8eb6
Oleks Bazurin <a.bazurin@gmail.com>
2023-03-14 03:18:24 -0600

    react components for CRUD actions

---
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {Activity} from "../../../app/models/activity";
+import {Button, Item, Label, Segment} from "semantic-ui-react";
+
+interface Props {
+    activities: Activity[];
+    selectActivity: (id: string) => void;
+    deleteActivity: (id: string) => void;
+}
+export default function ActivityList({activities, selectActivity, deleteActivity}: Props)
+{
+    return(
+        <Segment>
+            <Item.Group divided>
+                {activities.map(act => (
+                    <Item key={act.id}>
+                        <Item.Content>
+                            <Item.Header as='a'>{act.title}</Item.Header>
+                            <Item.Meta>{act.date}</Item.Meta>
+                            <Item.Description>
+                                <div>{act.description}</div>
+                                <div>{act.city}, {act.venue}</div>
+                            </Item.Description>
+                            <Item.Extra>
+                                <Button onClick={() => selectActivity(act.id)} floated='right' content='View' color='blue'/>
+                                <Button onClick={() => deleteActivity(act.id)} floated='right' content='DELETE' negative/>
+                                <Label basic content={act.category}/>
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                ))}
+            </Item.Group>
+        </Segment>
+    )
+}
