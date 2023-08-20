6f579b240ebb90c6cdf4829d1a5cebe6ebc7e943
Oleks Bazurin <a.bazurin@gmail.com>
2023-08-20 11:44:29 -0600

    400 errors handling

--- Reactivities/client-app/src/app/api/Agent.ts
+++ Reactivities/client-app/src/app/api/Agent.ts
@@ -1,5 +1,7 @@
-import axios, {AxiosResponse} from "axios";
+import axios, {AxiosError, AxiosResponse} from "axios";
 import {Activity} from "../models/activity";
+import {toast} from "react-toastify";
+import {router} from "../router/Routes";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -7,32 +9,58 @@ const sleep = (delay: number) => {
     })
 }
 
-axios.defaults.baseURL = 'http://'+document.URL.split("//")[1].split(":")[0]+':5000/api'
+axios.defaults.baseURL = 'http://' + document.URL.split("//")[1].split(":")[0] + ':5000/api'
 
 axios.interceptors.response.use(async response => {
-    try{    
-        await sleep(1000);
-        return response;
-    } catch(error) {
-        console.log(error);
-        return Promise.reject(error);
+    await sleep(1000);
+    return response;
+}, (error: AxiosError) => {
+    const {data, status} = error.response as AxiosResponse;
+    switch (status) {
+        case 400:
+            if(data.errors){
+                const modalStateErrors = [];
+                for (const key in data.errors){
+                    if (data.errors[key])
+                    {
+                        modalStateErrors.push(data.errors[key]);
+                    }
+                }
+                throw modalStateErrors.flat()
+            } else {
+                toast.error(data);
+            }
+            break;
+        case 401:
+            toast.error('unauthorised');
+            break;
+        case 403:
+            toast.error('forbiden');
+            break;
+        case 404:
+            router.navigate('/not-found')
+            break;
+        case 500:
+            toast.error('server error');
+            break;
     }
+    return Promise.reject(error);
 })
 
-const responseBody = <T> (response: AxiosResponse<T>) => response.data;
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
-    get: <T> (url: string) => axios.get<T>(url).then(responseBody),
-    post: <T> (url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
-    put: <T> (url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
-    delete: <T> (url: string) => axios.delete<T>(url).then(responseBody),
+    get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+    post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+    put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+    delete: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 }
 
 const Activities = {
     list: () => requests.get<Activity[]>('/activities'),
     details: (id: string) => requests.get<Activity>(`activities/${id}`),
-    create:  (act: Activity) => requests.post<void>('/activities', act),
-    update:  (act: Activity) => requests.put<void>(`/activities/${act.id}`, act),
+    create: (act: Activity) => requests.post<void>('/activities', act),
+    update: (act: Activity) => requests.put<void>(`/activities/${act.id}`, act),
     delete: (id: string) => axios.delete<void>(`activities/${id}`)
 }
 
--- Reactivities/client-app/src/app/router/Routes.tsx
+++ Reactivities/client-app/src/app/router/Routes.tsx
@@ -1,9 +1,10 @@
-import {createBrowserRouter, RouteObject} from "react-router-dom";
+import {createBrowserRouter, Navigate, RouteObject} from "react-router-dom";
 import App from  "../layout/App";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import TestErrors from "../../features/Errors/TestError";
+import NotFound from "../../features/Errors/NotFound";
 export const routes: RouteObject[] = [
     {
         path: '/',
@@ -13,7 +14,9 @@ export const routes: RouteObject[] = [
             {path: 'activities', element: <ActivityDashboard />},
             {path: 'createActivity', element: <ActivityForm key='create'/>},
             {path: 'manage/:id', element: <ActivityForm key='manage'/>},
-            {path: 'errors', element: <TestErrors key='errors'/>}
+            {path: 'errors', element: <TestErrors key='errors'/>},
+            {path: 'not-found', element: <NotFound key='errors'/>},
+            {path: '*', element: <Navigate replace to='/not-found'/>}
         ]
     }
 ]
