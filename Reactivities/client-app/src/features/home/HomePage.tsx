5322caa1ef54fa901fe9648eaf8c63e49ca7430f
Oleks Bazurin <a.bazurin@gmail.com>
2023-08-20 23:15:44 -0600

    add version label on a home page

--- Reactivities/client-app/src/features/home/HomePage.tsx
+++ Reactivities/client-app/src/features/home/HomePage.tsx
@@ -1,10 +1,13 @@
-import {Button, Container, Header, Image, Segment} from "semantic-ui-react";
+import {Button, Container, Header, Image, Label, Segment} from "semantic-ui-react";
 import {Link} from "react-router-dom";
 
 export default function HomePage(){
     return(
        <Segment inverted textAlign='center' vertical className='masthead'>
            <Container text>
+               <Label color='blue'>
+                   Version 0.11.1
+               </Label>
                <Header as='h1' inverted>
                    <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom: 12}}/>
                    Reactivities!
